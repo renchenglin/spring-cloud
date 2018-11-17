6fc07a597e04378a21abda6c2fea20baef99caed
Administrator <Administrator@USER-20180601UZ>
2018-11-17 20:25:39 +0800

    l

--- spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker         // 开启断路器功能，进行容错管理
+@EnableDiscoveryClient        // 开启服务发现功能
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
