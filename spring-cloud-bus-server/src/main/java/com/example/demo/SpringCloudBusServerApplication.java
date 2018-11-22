543c1f9a1e46113f4d28a154d583ac73d173e23f
Administrator <Administrator@USER-20180601UZ>
2018-11-22 22:37:32 +0800

    bus

---
+++ spring-cloud-bus-server/src/main/java/com/example/demo/SpringCloudBusServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudBusServerApplication.class, args);
+	}
+}
